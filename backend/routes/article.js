38b5a496a499b74817f06bf85dd5f1b46bae1ad0
walid hamdi <walidhamdidev@gmail.com>
2023-04-27 11:53:49 +0100

    structure the project / backend / frontend

--- backend/routes/article.js
+++ backend/routes/article.js
@@ -4,15 +4,11 @@ import article from "../controllers/article.js";
 import fileUploadMiddleware from "../middleware/upload.js";
 
 const router = express.Router();
-// get one
+
 router.get("/:id", article.getOne);
-// get all
 router.get("/", article.getAll);
-// add article
 router.post("/", fileUploadMiddleware(), article.addArticle);
-// delete
 router.delete("/:id", article.deleteArticle);
-// update
-router.put("/:id", article.update);
+router.put("/:id", fileUploadMiddleware(), article.update);
 
 export default router;
