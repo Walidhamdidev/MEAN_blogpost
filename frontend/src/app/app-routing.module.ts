a53ca52b642e1d2b1f1d50a9c2f97ae4e0b5b14a
walid hamdi <walidhamdidev@gmail.com>
2023-04-27 23:34:05 +0100

    structure frontend folders and add template components

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { AuthorComponent } from './pages/author/author.component';
+import { ArticledetailComponent } from './pages/articledetail/articledetail.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { PrivacyComponent } from './pages/privacy/privacy.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ArchiveComponent } from './pages/archive/archive.component';
+import { CreatearticleComponent } from './pages/createarticle/createarticle.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'archive', component: ArchiveComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'author', component: AuthorComponent },
+  { path: 'createarticle', component: CreatearticleComponent },
+  { path: 'articledetails', component: ArticledetailComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'privacy', component: PrivacyComponent },
+  { path: '**', component: NotfoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { AboutComponent } from './pages/about/about.component';
+import { AuthorComponent } from './pages/author/author.component';
+import { CreatearticleComponent } from './pages/createarticle/createarticle.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ArticledetailComponent } from './pages/articledetail/articledetail.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { NotfoundComponent } from './pages/notfound/notfound.component';
+import { PrivacyComponent } from './pages/privacy/privacy.component';
+import { CoverComponent } from './pages/home/cover/cover.component';
+import { BlogListComponent } from './pages/home/blog-list/blog-list.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ArchiveComponent } from './pages/archive/archive.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, AboutComponent, AuthorComponent, CreatearticleComponent, HomeComponent, ArticledetailComponent, LoginComponent, RegisterComponent, NotfoundComponent, PrivacyComponent, CoverComponent, BlogListComponent, ContactComponent, ArchiveComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
